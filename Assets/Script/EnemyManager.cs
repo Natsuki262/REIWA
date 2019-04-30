0a5b898227922816b69f347e137bd01cb99f55e8
NatsukiFGL <hiro.58.fgl.2525.952@icloud.com>
2019-04-30 23:47:48 +0900

    【add】エネミーヘイセイの生成マネージャーを作成

--- Assets/Script/EnemyManager.cs
+++ Assets/Script/EnemyManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    
+    public GameObject enemy_heisei;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Arrival", 1, 1);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       // Arrival();
+    }
+    void Arrival()  //敵の出現関数
+    {
+        Instantiate(enemy_heisei, new Vector3(-5f + 8 * Random.value, 10, 0), Quaternion.identity);
     }
 }
